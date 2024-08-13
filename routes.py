5bbe8652a0dd996f2f3b5dda264f3e48de2dbb97
KhanradCoder <akhanrade@gmail.com>
2024-08-13 11:26:53 -0400

    fix for prod

--- routes.py
+++ routes.py
@@ -1,14 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, send_file, request, session, current_app
+from app.boilerplateGenerator import create_boilerplate
 import stripe
 import shutil
-from app.boilerplateGenerator import create_boilerplate
 
 main = Blueprint('main', __name__)
-
-@main.before_app_first_request
-def setup_stripe():
-    # Set Stripe API key
-    stripe.api_key = current_app.config['STRIPE_SECRET_KEY']
 
 @main.route('/')
 def index():
@@ -16,22 +11,20 @@ def index():
 
 @main.route('/build')
 def build():
-    template_themes = {
-        'darkly': 'https://bootswatch.com/5/darkly/bootstrap.css',
-        'flatly': 'https://bootswatch.com/5/flatly/bootstrap.css',
-        'journal': 'https://bootswatch.com/5/journal/bootstrap.css',
-        'litera': 'https://bootswatch.com/5/litera/bootstrap.css',
-        'lux': 'https://bootswatch.com/5/lux/bootstrap.css',
-        'minty': 'https://bootswatch.com/5/minty/bootstrap.css',
-        'quartz': 'https://bootswatch.com/5/quartz/bootstrap.css',
-        'solar': 'https://bootswatch.com/5/solar/bootstrap.css',
-        'vapor': 'https://bootswatch.com/5/vapor/bootstrap.css'
-    }
+    template_themes = {'darkly':'https://bootswatch.com/5/darkly/bootstrap.css',
+                       'flatly':'https://bootswatch.com/5/flatly/bootstrap.css',
+                       'journal':'https://bootswatch.com/5/journal/bootstrap.css',
+                       'litera':'https://bootswatch.com/5/litera/bootstrap.css',
+                       'lux':'https://bootswatch.com/5/lux/bootstrap.css',
+                       'minty':'https://bootswatch.com/5/minty/bootstrap.css',
+                       'quartz':'https://bootswatch.com/5/quartz/bootstrap.css',
+                       'solar':'https://bootswatch.com/5/solar/bootstrap.css',
+                       'vapor':'https://bootswatch.com/5/vapor/bootstrap.css'}
     return render_template('build.html', themes=template_themes.keys())
 
 @main.route('/preview', methods=['POST'])
 def preview():
-    app_name = request.form['app_name']
+    app_name = request.form['app_name'] 
     app_tagline = request.form['app_tagline']
     app_description = request.form['app_description']
     app_theme = request.form['app_theme']
@@ -47,11 +40,19 @@ def preview():
 
 @main.route('/create-checkout-session', methods=['POST'])
 def create_checkout_session():
+    # Ensure this route runs within the application context
+    stripe_keys = {
+        "secret_key": current_app.config['STRIPE_SECRET_KEY'],
+        "publishable_key": current_app.config['STRIPE_PUBLISHABLE_KEY'],
+        "price": current_app.config['STRIPE_PRICE_ID']
+    }
+    stripe.api_key = stripe_keys["secret_key"]
+
     try:
         checkout_session = stripe.checkout.Session.create(
             line_items=[
                 {
-                    'price': current_app.config['STRIPE_PRICE_ID'],  # Access price_id directly from config
+                    'price': stripe_keys["price"],
                     'quantity': 1,
                 },
             ],
@@ -66,11 +67,11 @@ def create_checkout_session():
 
 @main.route('/success')
 def success():
-    app_name = session.get('app_name', 'Unknown App')
-    app_tagline = session.get('app_tagline', 'No Tagline')
-    app_description = session.get('app_description', 'No Description')
-    app_theme = session.get('app_theme', 'No Theme')
-    app_mode = session.get('app_mode', 'No Mode')
+    app_name = session['app_name']
+    app_tagline = session['app_tagline']
+    app_description = session['app_description']
+    app_theme = session['app_theme']
+    app_mode = session['app_mode']
 
     return render_template('success.html', app_name=app_name, app_tagline=app_tagline, app_description=app_description, app_theme=app_theme, app_mode=app_mode)
 
@@ -80,13 +81,13 @@ def cancel():
 
 @main.route('/download', methods=['POST'])
 def download():
-    app_name = session.get('app_name', 'Unknown App')
-    app_tagline = session.get('app_tagline', 'No Tagline')
-    app_description = session.get('app_description', 'No Description')
-    app_theme = session.get('app_theme', 'No Theme')
-    app_mode = session.get('app_mode', 'No Mode')
+    app_name = session['app_name']
+    app_tagline = session['app_tagline']
+    app_description = session['app_description']
+    app_theme = session['app_theme']
+    app_mode = session['app_mode']
 
     # Zip boilerplate code
     temp_dir = create_boilerplate(app_name, app_tagline, app_description, app_theme, app_mode)
     zip_path = shutil.make_archive(temp_dir, 'zip', temp_dir)
-    return send_file(zip_path, as_attachment=True, download_name=app_name.strip().replace(" ", "_") + ".zip")
+    return send_file(zip_path, as_attachment=True, download_name=app_name.strip().replace(" ", "_")+".zip")
